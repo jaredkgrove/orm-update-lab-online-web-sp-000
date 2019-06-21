71aec361c56f271fb1c1d5426b78ebf1f5896b7d
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 19:54:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,13 +34,12 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL 
+    sql = <<-SQL
       SELECT *
       FROM students
       WHERE name = ?
     SQL
     DB[:conn].execute(sql, name).collect{|row| self.new_from_db(row)}.first
-
   end
 
   def self.new_from_db(row)
