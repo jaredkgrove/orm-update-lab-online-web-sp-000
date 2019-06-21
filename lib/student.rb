ef09039a5fe9435c0eb9d35cee3ccabc0c08aa57
Jared Grove <jaredkgrove@gmail.com>
2019-06-21 19:34:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,4 +11,12 @@ class Student
     @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+  end
 end
